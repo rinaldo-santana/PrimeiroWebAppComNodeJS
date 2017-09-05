8d4414671731ea2cfb5957ee4e9504458854a6d2
rinaldo-santana <rinaldo.rms@gmail.com>
2017-09-05 18:45:17 -0300

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+  	load = require('express-load'),
+  	error = require('./middleware/error');
+  	app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.set('/')
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
+  
+app.listen(3000, function(){
+  console.log("Ntalk rodando no servidor...");
+})
+
---
+++ routes/home.js
@@ -0,0 +1,11 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(app){
+	var home = app.controllers.home;
+	app.get('/', home.index);
+	app.post('/entrar', home.login);
+	app.get('/sair', home.logout);
+};
